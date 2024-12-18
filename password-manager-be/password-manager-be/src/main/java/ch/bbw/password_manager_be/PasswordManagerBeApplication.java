fc8467b0387c39ce52e6399de8ed271a4bc7bc29
AimceptionGian <67605487+AimceptionGian@users.noreply.github.com>
2024-12-18 13:58:19 +0100

    created frontend and backend

---
+++ password-manager-be/password-manager-be/src/main/java/ch/bbw/password_manager_be/PasswordManagerBeApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.password_manager_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordManagerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordManagerBeApplication.class, args);
+	}
+
+}
