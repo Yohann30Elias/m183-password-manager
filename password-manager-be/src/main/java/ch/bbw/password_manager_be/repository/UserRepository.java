b58ded22545afc2de5b4f3f66d48033cddd03a09
AimceptionGian <67605487+AimceptionGian@users.noreply.github.com>
2025-01-08 13:49:50 +0100

    created UserRepository

---
+++ password-manager-be/src/main/java/ch/bbw/password_manager_be/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package ch.bbw.password_manager_be.repository;
+
+import ch.bbw.password_manager_be.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
